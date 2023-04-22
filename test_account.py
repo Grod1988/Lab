3a7775978d30cf73e3492a8c2b0a01fdf7e45752
Grod1988 <131494632+Grod1988@users.noreply.github.com>
2023-04-21 21:11:59 -0500

    Create test account

---
+++ test_account.py
@@ -0,0 +1,27 @@
+from account import Account
+
+def test_init():
+    account = Account("John")
+    assert account.get_name() == "John"
+    assert account.get_balance() == 0
+
+def test_deposit():
+    account = Account("John")
+    assert account.deposit(100) == True
+    assert account.get_balance() == 100
+    assert account.deposit(-50) == False
+    assert account.get_balance() == 100
+    assert account.deposit(0) == False
+    assert account.get_balance() == 100
+
+def test_withdraw():
+    account = Account("John")
+    account.deposit(100)
+    assert account.withdraw(50) == True
+    assert account.get_balance() == 50
+    assert account.withdraw(-25) == False
+    assert account.get_balance() == 50
+    assert account.withdraw(0) == False
+    assert account.get_balance() == 50
+    assert account.withdraw(100) == False
+    assert account.get_balance() == 50
